df30120332fc436a680e65737759fdbf117a38b9
cantelope <s.r.mcgann@hotmail.com>
2022-01-27 14:05:25 -0800

    sync

--- public/links/curWalletLink.php
+++ public/links/curWalletLink.php
@@ -1,6 +1,6 @@
 <?
   require('../db.php');
-  $output = shell_exec($command = "sudo -u cantelope /usr/local/bin/ipfs files stat /sync 2>&1");
+  $output = shell_exec($command = "sudo -u cantelope /usr/local/bin/ipfs files stat /syncWallets 2>&1");
   $file = explode(' ',shell_exec($command = "sudo -u cantelope /usr/local/bin/ipfs ls " .  explode("\n", $output)[0]))[2];
   echo explode("\n", $ipfsURL . explode("\n", $output)[0] . '/' . $file)[0];
 ?>
