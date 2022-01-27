7fbbc849955729f00621fb92cf11b18268c04fde
cantelope <s.r.mcgann@hotmail.com>
2022-01-26 17:11:00 -0800

    sync

--- public/db.php
+++ public/db.php
@@ -4,8 +4,8 @@
   $db_host="localhost";
   $db="generative";
   $maxResultsPerPage = 4;
-  $peer = 'https://plorg.net';
-  $baseURL = 'plorgmirror.appliedlearning.academy';
-  $ipfsURL = 'https://ipfs.appliedlearning.academy/ipfs/';  
+  $peer = 'https://plorgmirror.appliedlearning.academy';
+  $baseURL = 'plorg.net';
+  $ipfsURL = 'https://ipfs.dweet.net/ipfs/';  
   $link = mysqli_connect($db_host, $db_user, $db_pass, $db);
 ?>
